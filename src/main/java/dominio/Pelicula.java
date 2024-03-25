fc3cc7ce5c0b3e5b33df2b6a9e12fe814350acb7
Manuel Lopez <aqnuelmagamer8@gmail.com>
2024-03-25 11:40:14 -0600

    creacion de la clase pelicula

---
+++ src/main/java/dominio/Pelicula.java
@@ -0,0 +1,49 @@
+package dominio;
+
+import java.util.Objects;
+
+public class Pelicula {
+    String nombre;
+    public Pelicula(){}
+    public Pelicula(String nombre) {
+        this.nombre = nombre;
+    }
+
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Pelicula pelicula = (Pelicula) o;
+
+        return Objects.equals(nombre, pelicula.nombre);
+    }
+
+    @Override
+    public int hashCode() {
+        return nombre != null ? nombre.hashCode() : 0;
+    }
+
+    @Override
+    public String toString() {
+        return "Pelicula{" +
+                "nombre='" + nombre + '\'' +
+                '}';
+    }
+
+    public static void main(String[] args) {
+        Pelicula pelicula1 = new Pelicula("superman");
+        Pelicula pelicula2 = new Pelicula("batman");
+        System.out.println(pelicula1);
+        System.out.println(pelicula2);
+    }
+}
